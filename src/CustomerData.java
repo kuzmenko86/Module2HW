c3ba86b3070cfe7df4ba0c77c1decc915ca03782
kuzmenko86 <kuzmenko86@gmail.com>
2017-06-30 20:45:24 +0300

    final version

--- src/CustomerData.java
+++ src/CustomerData.java
@@ -12,9 +12,20 @@ public class CustomerData {
         System.out.println("Введите город");
         String city = sc.nextLine();
 
-        System.out.println("Введите возраст");
-        int age = sc.nextInt();
-        sc.nextLine();//java bug
+        int age;
+        //sc.nextLine();//java bug
+
+        while (true) {
+            try {
+                System.out.println("Введите возраст");
+                age = sc.nextInt();
+                sc.nextLine();//java bug
+                break;
+            } catch (Exception e) {
+                System.out.println("Упс, это не число!!! может все таки - ");
+                sc.nextLine();//java bug
+            }
+        }
 
         System.out.println("Введите hobby");
         String hobby = sc.nextLine();
@@ -25,13 +36,14 @@ public class CustomerData {
         System.out.println("Город: " + city);
         System.out.println("Возраст: " + age);
         System.out.println("Хобби: " + hobby);
-        System.out.println("===================: " + name);
-
-        System.out.println("***** Вариант 2 (текстовый):" + name);
+        System.out.println("===================: ");
+        System.out.println();
+        
+        System.out.println("***** Вариант 2 (текстовый):");
         System.out.println("Человек по имени " + name + " живет в городе " + city);
         System.out.println("Этому человеку " + age + " лет и любит он заниматься " + hobby);
         System.out.println("===================: ");
-
+        System.out.println();
 
         System.out.println(" ***** Вариант 3 (иной): ");
         System.out.println(name + " - Имя");
@@ -39,6 +51,7 @@ public class CustomerData {
         System.out.println(age + " - Возраст");
         System.out.println(hobby + " - Хобби");
         System.out.println("======== E N D =========== ");
+        System.out.println();
 
     }
 }
